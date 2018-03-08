b69b3bde7bbbb2ee98ef70c3dbb04379731aa5ab
Press01 <37179365+press01@users.noreply.github.com>
2018-03-08 16:21:49 +0300

    <maven.compiler.source>1.6</maven.compiler.source>         <maven.compiler.target>1.6</maven.compiler.target>     </properties>     <build>         <plugins>             <plugin>                 <artifactId>maven-assembly-plugin</artifactId>                 <configuration>                     <archive>                         <manifest>                             <mainClass>ru.example.helloworld.HelloWorld</mainClass>                         </manifest>                     </archive>                     <descriptorRefs>                         <descriptorRef>jar-with-dependencies</descriptorRef>                     </descriptorRefs>                 </configuration>             </plugin>         </plugins>     </build>

---
+++ helloworld/src/main/java/ru/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.example.helloworld;
+
+/**
+ * Hello world class
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello!");
+    }
+}
